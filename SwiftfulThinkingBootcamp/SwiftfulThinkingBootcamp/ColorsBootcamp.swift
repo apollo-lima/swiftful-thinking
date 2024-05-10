c14fe2685bb1415a47d6922bc963a96be4c8bfb9
peoux <apollolima@icloud.com>
2024-05-10 22:19:48 +0100

    Shapes & Colors

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ColorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Apollo Lima on 10/05/2024.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsBootcamp()
+}
