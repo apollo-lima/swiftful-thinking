fffe9776533fbe4f9de6672bb9892f52545cc2be
peoux <apollolima@icloud.com>
2024-05-10 22:25:17 +0100

    Shapes & Colors

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ColorsBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ColorsBootcamp.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ColorsBootcamp: View {
+    // create a variable, then open Color(#colorLiteral()) then just either add this to the Color or copy to get the RGB
+    var colour = Color(#colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1))
+    
+    // For custom colors in light and dark mode you have to add a custom color in the assets
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25)
+            .fill(
+//                Color.primary
+//                Color(#colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1))
+//                Color(UIColor.secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+//            .shadow(radius: 50)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -20, y: -20)
     }
 }
 
--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ShapesBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ShapesBootcamp.swift
@@ -1,15 +1,22 @@
-//
-//  ShapesBootcamp.swift
-//  SwiftfulThinkingBootcamp
-//
-//  Created by Apollo Lima on 10/05/2024.
-//
-
 import SwiftUI
 
 struct ShapesBootcamp: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        Circle()
+//        Ellipse()
+//        Capsule(style: .circular)
+//        Rectangle()
+        RoundedRectangle(cornerRadius: 50)
+//            .fill(Color.red)
+//            .foregroundColor(.green)
+//            .stroke()
+//            .stroke(Color.red)
+//            .stroke(Color.red, lineWidth: 30)
+//            .stroke(Color.orange, style: StrokeStyle(lineWidth: 30, lineCap: CGLineCap.butt, dash: [30]))
+            .trim(from: 0.4, to: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+//            .stroke(Color.purple, lineWidth: 50)
+            .frame(width: 300, height: 100, alignment: .leading)
     }
 }
 
