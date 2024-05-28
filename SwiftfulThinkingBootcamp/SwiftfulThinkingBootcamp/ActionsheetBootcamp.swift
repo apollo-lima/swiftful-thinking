35d79475d394e75b2119271eb9cc50c59c3c69bb
peoux <apollolima@icloud.com>
2024-05-28 21:25:04 +0100

    ActionSheet (ConfirmationDialog) and Context Menu

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ActionsheetBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ActionsheetBootcamp.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ActionsheetBootcamp: View {
+    
+    @State var showConfirmationDialog: Bool = false
+    @State var actionSheetOption: ActionSheetOptions = .isOtherPost
+    
+    // ActionSheet is now ConfirmationDialog
+    
+    enum ActionSheetOptions {
+        case isMypost
+        case isOtherPost
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                Text("@username")
+                Spacer()
+                Button(action: {
+                    actionSheetOption = .isOtherPost
+                    showConfirmationDialog.toggle()
+                }, label: {
+                    Image(systemName: "ellipsis")
+                })
+                .tint(.primary)
+            }
+            .padding(.horizontal)
+            
+            Rectangle()
+                .aspectRatio(1.0, contentMode: .fit)
+        }
+        .confirmationDialog("This is the title",
+                            isPresented: $showConfirmationDialog,
+                            titleVisibility: .visible) {
+            getButtons()
+        } message: {
+            Text("This is the message")
+        }
+    }
+}
+
+func getButtons() -> some View {
+    return Group {
+        Button("Change Profile") {}
+        Button("Log Out", role: .destructive) {}
+        Button("Cancel", role: .cancel) {}
     }
 }
 
--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ContextMenuBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/ContextMenuBootcamp.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct ContextMenuBootcamp: View {
+    
+    @State var backgroundColor: Color = Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 10.0) {
+            Image(systemName: "house.fill")
+                .font(.title)
+            Text("Swiftful Thinking")
+                .font(.headline)
+            Text("How to use Context Menu")
+                .font(.headline)
+        }
+        .foregroundStyle(.white)
+        .padding(30)
+        .background(Color(backgroundColor).clipShape(RoundedRectangle(cornerRadius: 30)))
+        .contextMenu(menuItems: {
+            Button(action: {
+                backgroundColor = .yellow
+            }, label: {
+                Label("Share Post", systemImage: "flame.fill")
+            })
+            
+            Button(action: {
+                backgroundColor = .red
+            }, label: {
+                Text("Report post")
+            })
+            
+            Button(action: {
+                backgroundColor = .green
+            }, label: {
+                HStack {
+                    Text("Like post")
+                    Image(systemName: "heart.fill")
+                }
+                
+            })
+        })
+        
+//        var colour = Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
     }
 }
 
