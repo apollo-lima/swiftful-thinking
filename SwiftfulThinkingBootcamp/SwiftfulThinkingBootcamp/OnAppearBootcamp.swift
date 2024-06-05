f7380babbd51f35bb4697c1f7232ebfb4ef0d6f7
peoux <apollolima@icloud.com>
2024-06-05 19:06:41 +0100

    OnAppear

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Apollo Lima on 05/06/2024.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnAppearBootcamp()
+}
