6a26596a0b78b83582dbb73d8bf7926a3a5d73ce
peoux <apollolima@icloud.com>
2024-06-05 20:50:41 +0100

    OnAppear

--- SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/OnAppearBootcamp.swift
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/OnAppearBootcamp.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct OnAppearBootcamp: View {
+    // MARK: PROPERTIES
+    @State var myText: String = "Start text."
+    @State var count: Int = 0
+    
+    // MARK: BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                Text(myText)
+                LazyVStack {
+                    ForEach(0..<50) { _ in
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear {
+                                count += 1
+                            }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "This is the new text!"
+                }
+                })
+            .onDisappear(perform: {
+                myText = "Ending text."
+            })
+            .navigationTitle("On Appear \(count)")
+        }
     }
 }
 
+
+// MARK: PREVIEW
 #Preview {
     OnAppearBootcamp()
 }
